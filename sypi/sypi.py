b0479953bf19777a50dc28d27af3f45c894399b5
Sergio Romero Barra <s.romerobarra@gmail.com>
2016-08-04 18:09:50 +0200

    Added file logging.

--- sypi/sypi.py
+++ sypi/sypi.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+from sypi import utils
 from sypi import info
 
 class Sypi():
+
+	def __init__(self, log_path="./logs", logging_level='debug'):
+
+		self._log_path = log_path
+		self._log_file = "{path}/{file_name}.log".format(path=self._log_path, file_name=__name__)
+		self._logging_level = logging_level
+		self._logger = utils.config_logger(__name__, self._log_file, logging_level=self._logging_level)
+
+		self._logger.debug("{name} object instantiation".format(name=__name__))
 
 	def info(self):
 
